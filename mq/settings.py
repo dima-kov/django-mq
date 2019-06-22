621c4f30fd81bb7ff9579353cf7cce39a0244bd4
Dima Kovalchuk <dima.kovalchuk.v@gmail.com>
2019-06-22 13:44:01 +0300

    Drop print

--- mq/settings.py
+++ mq/settings.py
@@ -3,5 +3,3 @@ from django.conf import settings
 MQ_REDIS_HOST = getattr(settings, 'MQ_REDIS_HOST', None)
 
 MQ_REDIS_PORT = getattr(settings, 'MQ_REDIS_PORT', None)
-
-print(settings.INSTALLED_APPS)
