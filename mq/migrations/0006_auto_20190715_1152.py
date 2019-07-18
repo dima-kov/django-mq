901f0298fc11274ab895a75146c416dc3cfa4d6c
Dima Kovalchuk <dima.kovalchuk.v@gmail.com>
2019-07-18 12:38:22 +0300

    Change Migration: to delete all errors

--- mq/migrations/0006_auto_20190715_1152.py
+++ mq/migrations/0006_auto_20190715_1152.py
@@ -4,16 +4,22 @@ from django.db import migrations, models
 import django.db.models.deletion
 
 
-class Migration(migrations.Migration):
+def delete_all_errors(apps, schema_editor):
+    MqError = apps.get_model("mq", "MqError")
+    MqError.objects.delete()
 
+
+class Migration(migrations.Migration):
     dependencies = [
         ('mq', '0005_mqmessagetype'),
     ]
 
     operations = [
+        migrations.RunPython(delete_all_errors),
         migrations.AlterField(
             model_name='mqerror',
             name='message_type',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='mq_errors', to='mq.MqMessageType', verbose_name='Message Type'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='mq_errors',
+                                    to='mq.MqMessageType', verbose_name='Message Type'),
         ),
     ]
