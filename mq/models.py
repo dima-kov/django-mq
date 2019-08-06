771764e77f7fe7e16e5ac89eee73fd370fb48b7f
Dima Kovalchuk <dima.kovalchuk.v@gmail.com>
2019-08-06 19:00:10 +0300

    MqQueueItem model

--- mq/models.py
+++ mq/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext as _
 
 
@@ -62,23 +63,73 @@ class MqError(models.Model):
         return _("Error {}").format(self.raised_at)
 
 
-CREATED = 10
-IN_QUEUE = 20
-IN_PROCESS = 30
-SUCCEED = 40
-ERROR = 50
+class MqQueueItem(models.Model):
+    CREATED = 10
+    IN_QUEUE = 20
+    IN_PROCESS = 30
+    SUCCEED = 40
+    ERROR = 50
 
-MQ_STATUS_CHOICES = (
-    (CREATED, _('Створено')),
-    (IN_QUEUE, _('В черзі')),
-    (IN_PROCESS, _('В процесі')),
-    (SUCCEED, _('Оброблено')),
-    (ERROR, _('Помилка під час обробки')),
-)
+    MQ_STATUS_CHOICES = (
+        (CREATED, _('Створено')),
+        (IN_QUEUE, _('В черзі')),
+        (IN_PROCESS, _('В процесі')),
+        (SUCCEED, _('Оброблено')),
+        (ERROR, _('Помилка під час обробки')),
+    )
+    # To use statues, just import and concatenate with custom:
+    # STATUS_CHOICES = MQ_STATUS_CHOICES + (
+    #   (100, _('Custom'),
+    # )
+    # All integers up to 100, is reserved by mq. Do no use them
+    # in case of future mq updates
 
-# To use statues, just import and concatenate with custom:
-# STATUS_CHOICES = MQ_STATUS_CHOICES + (
-#   (100, _('Custom'),
-# )
-# Integers up to 100, is reserved by mq. Do no use them
-# in case of future mq updates
+    in_queue_at = models.DateTimeField(
+        verbose_name=_('В черзі о'),
+        null=True,
+        blank=True,
+    )
+    in_process_at = models.DateTimeField(
+        verbose_name=_('Почато обробку о'),
+        null=True,
+        blank=True,
+    )
+    succeed_at = models.DateTimeField(
+        verbose_name=_('Оброблено о'),
+        null=True,
+        blank=True,
+    )
+    mq_status = models.CharField(
+        verbose_name=_('Статус'),
+        max_length=2,
+        choices=MQ_STATUS_CHOICES,
+        default=CREATED,
+    )
+
+    class Meta:
+        abstract = True
+
+    def in_process(self, commit=False):
+        self.status = self.IN_PROCESS
+        self.in_process_at = timezone.now()
+        if commit:
+            self.save(update_fields=['status', 'in_process_at'])
+
+    def error(self, commit=False):
+        self.status = self.ERROR
+        if commit:
+            self.save(update_fields=['status'])
+
+    def succeed(self, commit=True):
+        self.status = self.SUCCEED
+        self.succeed_at = timezone.now()
+        if commit:
+            self.save(update_fields=['status', 'succeed_at'])
+
+    @property
+    def is_succeed(self):
+        return self.status == self.SUCCEED
+
+    @property
+    def is_error(self):
+        return self.status == self.ERROR
