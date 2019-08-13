c0f3f853b74f258c12fb347d2f0890ebcb6e7d9e
Dima Kovalchuk <dima.kovalchuk.v@gmail.com>
2019-08-13 14:15:43 +0300

    Add test for queue push

--- mq/queue/queue/tests/test_abstract_queue.py
+++ mq/queue/queue/tests/test_abstract_queue.py
@@ -1,21 +1,25 @@
 from django.test import TestCase
 
 from mq.queue.messages import MessageType
-from mq.queue.queue.abstract import AbstractQueue
+from mq.queue.queue.redis import RedisQueue
+
+message_type_name_1 = 'type_1'
+message_type_name_2 = 'type_2'
+
+
+class Queue1(RedisQueue):
+    type_1 = MessageType(message_type_name_1)
+    type_2 = MessageType(message_type_name_2)
+
+    handled_types = (type_1, type_2)
 
 
 class AbstractQueueTestCase(TestCase):
 
     def test_check_handled_type(self):
-        message_type_name_1 = 'type_1'
-        message_type_name_2 = 'type_2'
-
-        class Queue1(AbstractQueue):
-            type_1 = MessageType(message_type_name_1)
-            type_2 = MessageType(message_type_name_2)
-
-            handled_types = (type_1, type_2)
-
         q = Queue1()
-
         assert message_type_name_1 in q.handled_types
+
+    def test_push(self):
+        q = Queue1()
+        q.push_wait([])
