e325f3596213e779564a316db132c528f7c07288
Dima Kovalchuk <dima.kovalchuk.v@gmail.com>
2020-02-29 01:39:34 +0200

    Sync queue consumer/handler/worker

---
+++ mq/queue/handlers/sync.py
@@ -0,0 +1,38 @@
+import asyncio
+
+from mq.queue.common.loop import AsyncLoop
+from mq.queue.exceptions import TerminatedException
+from mq.queue.consumers import QueueConsumer
+from mq.queue.queue.abstract import AbstractQueue
+
+
+class SyncQueueHandler(object):
+    queue: AbstractQueue = None
+    consumer_class = SyncConsumer
+    consumer_logger = '{consumer_item}'
+
+    def __init__(self):
+        self.queue.processing_to_wait()
+
+    def handle(self):
+        cids = self.queue.consumers_register(1)
+        consumer = self.new_consumer(cids[0])
+
+        try:
+            print('Start handling')
+            consumer.consume()
+        except (KeyboardInterrupt, TerminatedException):
+            pass
+        finally:
+            print('Shutdown ...')
+            consumer.unregister()
+            print('Consumer unregistered')
+
+    def new_consumer(self, cid):
+        return self.consumer_class(cid=cid, **self.new_consumer_kwargs())
+
+    def new_consumer_kwargs(self):
+        return {
+            'queue': self.queue,
+            'logger_name': self.consumer_logger,
+        }
